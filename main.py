c1f9fd77f4a1ed1a5e50200d850cefd312db66cc
Muhamad Padli Haikal <padlihaikal10@gmail.com>
2026-08-27 19:12:33 +0700

    Add text extraction for TXT and JSON files

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pypdf import PdfReader
 import io
+import json
 
 app = FastAPI()
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
 
-    contents = await file.read()
+    contents = await file.read() 
 
     if file.content_type not in ("application/pdf", "text/plain", "application/json", "application/msword"):
         raise HTTPException(
             status_code=400, 
             detail="File type does not match."
             )
-    if file.content_type == "application/pdf":
+    elif file.content_type == "application/pdf":
         extracted_text = extract_text_from_pdf(contents)
         return {"file name": file.filename, "file size": len(contents), "file extracted text": extracted_text}
+    
+    elif file.content_type == "text/plain":
+        extracted_text = contents.decode('utf-8')
+        return{"file name": file.filename, "file size": len(contents), "file extracted text": extracted_text}
+    
+    elif file.content_type == "application/json":
+        extracted_text = json.loads(contents.decode('utf-8'))
+        return{"file name": file.filename, "file size": len(contents), "file extracted text": extracted_text}
+    
     else:
         return {"file name": file.filename, "file size": len(contents)}
 
-def extract_text_from_pdf(contents):
+def extract_text_from_pdf(change):
     page_list = []
-    reader = PdfReader(io.BytesIO(contents))
+    reader = PdfReader(io.BytesIO(change))
     
     for page in reader.pages:        
         text = page.extract_text()      
