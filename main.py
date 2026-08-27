63a6da5a4420cd5e556491e0abe4c4cc0ae2a8ca
Muhamad Padli Haikal <padlihaikal10@gmail.com>
2026-08-27 20:08:14 +0700

    Add DOCX text extraction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from pypdf import PdfReader
+from docx import Document
 import io
 import json
 
@@ -10,7 +11,7 @@ async def create_upload_file(file: UploadFile):
 
     contents = await file.read() 
 
-    if file.content_type not in ("application/pdf", "text/plain", "application/json", "application/msword"):
+    if file.content_type not in ("application/pdf", "text/plain", "application/json", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"):
         raise HTTPException(
             status_code=400, 
             detail="File type does not match."
@@ -26,7 +27,11 @@ async def create_upload_file(file: UploadFile):
     elif file.content_type == "application/json":
         extracted_text = json.loads(contents.decode('utf-8'))
         return{"file name": file.filename, "file size": len(contents), "file extracted text": extracted_text}
-    
+
+    elif file.content_type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        extracted_text = extract_text_from_docx(contents)
+        return {"file name": file.filename, "file size": len(contents), "file extracted text": extracted_text}
+
     else:
         return {"file name": file.filename, "file size": len(contents)}
 
@@ -38,4 +43,14 @@ def extract_text_from_pdf(change):
         text = page.extract_text()      
         page_list.append(text)
     full_text = ",".join(page_list)
+    return full_text
+
+def extract_text_from_docx(contents):
+    doc_list = []
+    doc = Document(io.BytesIO(contents))
+
+    for para in doc.paragraphs:
+        text = para.text
+        doc_list.append(text)
+    full_text = ",".join(doc_list)
     return full_text
